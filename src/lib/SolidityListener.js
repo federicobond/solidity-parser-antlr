d57cfdfbb487ade6621b2ccf7fb12a15f4017a0e
Bernardo Vieira <bernardo5@live.com.pt>
2019-06-19 16:51:37 +0100

    update src/lib

--- src/lib/SolidityListener.js
+++ src/lib/SolidityListener.js
@@ -749,6 +749,24 @@ SolidityListener.prototype.exitElementaryTypeNameExpression = function(ctx) {
 };
 
 
+// Enter a parse tree produced by SolidityParser#natSpecMultilineComment.
+SolidityListener.prototype.enterNatSpecMultilineComment = function(ctx) {
+};
+
+// Exit a parse tree produced by SolidityParser#natSpecMultilineComment.
+SolidityListener.prototype.exitNatSpecMultilineComment = function(ctx) {
+};
+
+
+// Enter a parse tree produced by SolidityParser#natSpec.
+SolidityListener.prototype.enterNatSpec = function(ctx) {
+};
+
+// Exit a parse tree produced by SolidityParser#natSpec.
+SolidityListener.prototype.exitNatSpec = function(ctx) {
+};
+
+
 // Enter a parse tree produced by SolidityParser#numberLiteral.
 SolidityListener.prototype.enterNumberLiteral = function(ctx) {
 };
